3c3986ddd3040e2e0389c71aa59b956a4d3d1a3c
Peter Higgins <higgi13425@yahoo.com>
2018-05-04 18:07:49 -0400

    saved file to xlsx

--- test_usethis.R
+++ test_usethis.R
@@ -5,13 +5,16 @@
 # in a new directory'test'
 # using git, using packrat
 # installed usethis
-
+# installed writexl as demo to write file
 library(usethis)
+library(writexl)
+
 use_git()
 usethis::use_github(higgi13425/test_usethis.git)
 
 
 data <- data.frame(1:10,11:20,21:30)
+write_xlsx(data, path = "file.xls")
 
 # in terminal: "git push -u origin master"
 # now use Git tab to Stage, Commit, and Push
