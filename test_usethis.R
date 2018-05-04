812a293cbdeb98f88c73aa258abd0b8d7f76c956
Peter Higgins <higgi13425@yahoo.com>
2018-05-04 17:57:38 -0400

    initial data

---
+++ test_usethis.R
@@ -0,0 +1,13 @@
+#test script
+# to test usethis package
+
+# initiated new project
+# in a new directory'test'
+# using git, using packrat
+# installed usethis
+
+library(usethis)
+use_git()
+usethis::use_github()
+
+data <- data.frame(1:10,11:20,21:30)
